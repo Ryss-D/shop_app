1544d67c460e5de1399e5cac0614fba1cfb2c41a
Ryss-D <70545185+Ryss-D@users.noreply.github.com>
2022-07-07 15:27:23 -0500

    feat: added first favorites functionality implementation

--- lib/screens/products_overiew_screen.dart
+++ lib/screens/products_overiew_screen.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/product_grid.dart';
+import '../providers/products.dart';
+
+enum FilterOptions {
+  Favorites,
+  All,
+}
 
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productsContainer = Provider.of<Products>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Shop Overview'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (selectedValue) {
+              if (selectedValue == FilterOptions.Favorites) {
+                productsContainer.showFavoritesOnly();
+                //..
+              } else {
+                productsContainer.showAll();
+
+                ///...
+              }
+            },
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                  child: Text('Only favorites'),
+                  value: FilterOptions.Favorites),
+              PopupMenuItem(child: Text('Show all'), value: FilterOptions.All),
+            ],
+          ),
+        ],
       ),
       body: ProductsGrid(),
     );
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -17,6 +17,10 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context);
+    //another way to use the provider is via Consumer, it have the advantage of
+    // we can only warap the especific part we want to rebuild
+    // in this case the fav icon, and for example we will use Provider.of just
+    // to load data the first time
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -34,14 +38,21 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(
-                product.isFavorite ? Icons.favorite : Icons.favorite_border),
-            onPressed: () {
-              product.toogleFavoriteStatus();
-            },
-            color: Theme.of(context).colorScheme.secondary,
-          ),
+          //here is the consumer implementation
+          leading: Consumer<Product>(builder: (context, product, child) {
+            //child argument is used when we have parths into the widget that dont
+            //want to rebuild when values change
+            return IconButton(
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              onPressed: () {
+                product.toogleFavoriteStatus();
+              },
+              color: Theme.of(context).colorScheme.secondary,
+            );
+            //We define it ouside and later use ti inside return expression
+            //child: Text()
+          }),
           title: Text(
             product.title,
             textAlign: TextAlign.center,
