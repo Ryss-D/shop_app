05b442015c0f4e6179e085f20e33738d414dda15
Ryss-D <70545185+Ryss-D@users.noreply.github.com>
2022-07-09 21:46:13 -0500

    feat: added basic form with navigation nodes

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,23 +1,52 @@
 import 'package:flutter/material.dart';
 
 class EditProductScreen extends StatefulWidget {
+  static const routeName = '/edit-product';
+
   @override
   _EditProductScreenState createState() => _EditProductScreenState();
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  // this allow us to manually managa the next button of keyboard configured
+  // on forma
+  final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
   @override
   build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Edit Product'),
       ),
-      body: Form(
-        child: ListView(
-          children: [
-            TextFormField(),
-            TextFormField(),
-          ],
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Title'),
+                textInputAction: TextInputAction.next,
+                // onFieldSubmitted will be callen when the submit button of
+                // keyboard pressed in this time, the next button
+                onFieldSubmitted: (value) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+              ),
+              TextFormField(
+                  decoration: InputDecoration(labelText: 'Price'),
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
+                  focusNode: _priceFocusNode,
+                  onFieldSubmitted: (value) {
+                    FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                  }),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Description'),
+                textInputAction: TextInputAction.next,
+                focusNode: _descriptionFocusNode,
+              ),
+            ],
+          ),
         ),
       ),
     );
