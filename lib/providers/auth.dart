5442a9c1ce910ee88087d2c2687ab658dc4c4c9d
Ryss-D <70545185+Ryss-D@users.noreply.github.com>
2022-07-16 20:43:43 -0500

    feat: improved the internal auth logic

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -4,39 +4,34 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Auth with ChangeNotifier {
-  String _token;
-  DateTime _expiryTime;
-  String _userId;
+//  String _token;
+  //DateTime _expiryTime;
+  //String _userId;
 
-  Future<void> signup(String email, String password) async {
-    // Api key is on configuration section on firebase
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
     final url = Uri.parse(
+        // Api key is on configuration section on firebase
         //TODO: create env files to move keys
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDEZ4z3xuRq9LOhR7AOAOZYiFJMYTyiNb0');
-    final response = http.post(
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDEZ4z3xuRq9LOhR7AOAOZYiFJMYTyiNb0');
+    final response = await http.post(
       url,
       body: json.encode(
         {
-          'email': email,
+          'emai': email,
           'password': password,
           'returnSecureToken': true,
         },
       ),
     );
+    print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
   }
 
   Future<void> login(String email, String password) async {
-    final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDEZ4z3xuRq9LOhR7AOAOZYiFJMYTyiNb0');
-    final response = http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
