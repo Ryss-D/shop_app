1d411cecdd6b0eb2a7802e302920a384f63711d8
Ryss-D <70545185+Ryss-D@users.noreply.github.com>
2022-07-22 12:40:39 -0500

    feat:Added animated slivers and containers for details and orders

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -11,49 +11,102 @@ class ProductDetailScreen extends StatelessWidget {
     final loadedProduct =
         Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(loadedProduct.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Hero(
-                //tag shoul be the same in page who lands and on page who
-                //invoke
+      //appBar: AppBar(
+      //title: Text(loadedProduct.title),
+      //),
+      //body: SingleChildScrollView(
+// we will add the behabior of non appbar shown ujti you scroll down on page
+      body: CustomScrollView(
+        // child: Column(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                loadedProduct.title,
+              ),
+              background: Hero(
                 tag: loadedProduct.id,
-                child: Image.network(
-                  loadedProduct.imageUrl,
-                  fit: BoxFit.cover,
+                child: Image.network(loadedProduct.imageUrl, fit: BoxFit.cover),
+              ),
+            ),
+          ),
+          //SliverList is just a listview as part of multiple slivers
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Container(
+                  height: 300,
+                  width: double.infinity,
                 ),
-              ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  '\$${loadedProduct.price}',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  width: double.infinity,
+                  child: Text(
+                    loadedProduct.description,
+                    textAlign: TextAlign.center,
+                    softWrap: true,
+                  ),
+                ),
+                SizedBox(
+                  height: 800,
+                ),
+              ],
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              '\$${loadedProduct.price}',
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              width: double.infinity,
-              child: Text(
-                loadedProduct.description,
-                textAlign: TextAlign.center,
-                softWrap: true,
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
+        //Column(
+        //children: [
+        //   Container(
+        //     height: 300,
+        //     width: double.infinity,
+        //     child: Hero(
+        //       //tag shoul be the same in page who lands and on page who
+        //       //invoke
+        //       tag: loadedProduct.id,
+        //       child: Image.network(
+        //         loadedProduct.imageUrl,
+        //         fit: BoxFit.cover,
+        //       ),
+        //     ),
+        //   ),
+        //   SizedBox(
+        //     height: 10,
+        //   ),
+        //   Text(
+        //     '\$${loadedProduct.price}',
+        //     style: TextStyle(
+        //       color: Colors.grey,
+        //       fontSize: 20,
+        //     ),
+        //   ),
+        //   SizedBox(
+        //     height: 10,
+        //   ),
+        //   Container(
+        //     padding: EdgeInsets.symmetric(horizontal: 10),
+        //     width: double.infinity,
+        //     child: Text(
+        //       loadedProduct.description,
+        //       textAlign: TextAlign.center,
+        //       softWrap: true,
+        //     ),
+        //   ),
+        // ],
       ),
     );
   }
